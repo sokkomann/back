7baf4ed1148fa11600d1e6cc2f2f3be5c255b1be
kmj <sokkomann@gmail.com>
2026-02-04 13:47:46 +0900

    init

---
+++ src/main/java/com/app/haetssal_jangteo/HaetssalJangteoApplication.java
@@ -0,0 +1,13 @@
+package com.app.haetssal_jangteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaetssalJangteoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaetssalJangteoApplication.class, args);
+    }
+
+}
